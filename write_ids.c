3e4c6410506865e4cbe63be2dda882985a9599a0
SidKarpe1 <134902885+SidKarpe1@users.noreply.github.com>
2024-09-05 16:59:05 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "SidKarpe1";
+    char STUDENT_ID[] = "sjk6612";
 
     return 0 ;
 }
